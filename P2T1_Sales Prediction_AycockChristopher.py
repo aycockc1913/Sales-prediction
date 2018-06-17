971ee268408603776e44e876ef7fb1a031db2925
Christopher Aycock <40344855+aycockc1913@users.noreply.github.com>
2018-06-17 19:22:29 -0400

    Add files via upload

---
+++ P2T1_Sales Prediction_AycockChristopher.py
@@ -0,0 +1,14 @@
+#CTI-110
+#P2T1-Sales Prediction
+#Aycock Christopher
+#06/17/2018
+
+#projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+# the profit as 23 percent of sales
+profit = total_sales * 0.23
+
+#profit
+print('The profit is $' , format(profit, ',.2f'))
+      
